58a0505926e24693b0d3f0c3799f0bc3f71158b3
mazlumerceylan <mazlumerceylan98@gmail.com>
2023-04-24 01:00:45 +0200

    add remove & filter items

--- js/app.js
+++ js/app.js
@@ -1,23 +1,36 @@
-const { createApp } = Vue
+const { createApp } = Vue;
 
-  createApp({
-    data() {
-        return {
-          todos: [
-            { text: "Fare la spesa", done: false },
-            { text: "Fare il bucato", done: true },
-            { text: "Pulire la casa", done: false }
-          ],
-        };
-      },
-    }
-    methods: {
-      
-      },
-      
-    computed: {
-      
+createApp({
+  data() {
+    return {
+      todos: [
+        { text: "Fare la spesa", done: false },
+        { text: "Fare il bucato", done: true },
+        { text: "Pulire la casa", done: false }
+      ],
+      newTodoText: ""
+    };
+  },
+  methods: {
+    addTodo() {
+      if (this.newTodoText.trim() !== "") {
+        this.todos.push({ text: this.newTodoText, done: false });
+        this.newTodoText = "";
       }
-    
-  });
-  }).mount('#app')
+    },
+    removeTodo(index) {
+      this.todos.splice(index, 1);
+    },
+    toggleDone(index) {
+      this.todos[index].done = !this.todos[index].done;
+    }
+  },
+  computed: {
+    filteredTodos() {
+      return this.todos.filter(todo => {
+        return !todo.done;
+      });
+    }
+  }
+
+}).mount('#app');
