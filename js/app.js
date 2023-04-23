2e3e5268bc748f1c9ef0fd2ad1074cf03042eade
mazlumerceylan <mazlumerceylan98@gmail.com>
2023-04-24 00:39:07 +0200

    add data

--- js/app.js
+++ js/app.js
@@ -2,6 +2,22 @@ const { createApp } = Vue
 
   createApp({
     data() {
-      
+        return {
+          todos: [
+            { text: "Fare la spesa", done: false },
+            { text: "Fare il bucato", done: true },
+            { text: "Pulire la casa", done: false }
+          ],
+        };
+      },
     }
+    methods: {
+      
+      },
+      
+    computed: {
+      
+      }
+    
+  });
   }).mount('#app')
